0b09b0a8b04f13802786e851fa68376a4361c8e5
matheus.gomes <matheus.gomes@spcbrasil.org.br>
2022-11-22 08:17:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/SideBar';
+
+import './global.css'
+import styles from './App.module.css'
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/matheus-nb.png",
+      name: "Matheus Anderson",
+      role: "Estágiário @ SPCBrasil"
+    },
+    content: [ 
+      {
+        type: "paragraph",
+        content: "Fala Galera!",
+      },
+      {
+        type: "paragraph",
+        content:  "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da RocketSeat",
+      },
+      {
+        type: "link",
+        content: "github/matheus-nb/projetoFake",
+      }
+    ],
+    publishedAt: new Date('2022-11-17 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @ RocketSeat"
+    },
+    content: [ 
+      {
+        type: "paragraph",
+        content: "Fala Matheus!",
+      },
+      {
+        type: "paragraph",
+        content: "Ótimo projeto desenvolvido, continua assim e bora DALE",
+      },
+      {
+        type: "link",
+        content: "github/diego3g/projetoFake",
+      }
+    ],
+    publishedAt: new Date('2022-11-18 06:00:00'),
+  }
+]
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/components/Post.jsx
@@ -0,0 +1,86 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import { Avatar } from './Avatar.jsx'
+import { Comment } from './Comment.jsx'
+
+import styles from './Post.module.css'
+
+
+const comments = [
+  1,
+  2,
+  3
+]
+
+export function Post({ author, publishedAt, content }) {
+
+  const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+
+          <Avatar src={author.avatarUrl}/>
+
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+
+        </div>
+
+        <time 
+          title={publishedDateFormated} 
+          datetime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map(line => {
+          if(line.type === 'paragraph') {
+            return (
+              <p>{line.content}</p>
+            )
+          } else if (line.type === 'link') {
+            return (
+              <p>
+                <a href="">{line.content}</a>
+              </p>
+            )
+          }
+        })}
+      </div>
+
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return <Comment />
+        })}
+      </div>
+    </article>
+  )
+}
